09c4a9fe479e03e1c0f062c55b484a0edd2fb6bc
Vaibhav Srivastava <vaibhavsri9637@gmail.com>
2024-11-04 22:20:20 +0530

    Nov 04 changes done

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import entries from './entries.json';
 function App() {
 
   const [messagesSent,setMessagesSent] = useState(0);
-  const [startRollNumber,setStartRollNumber] = useState(entries[0]['Roll No.']);
+  const [startRollNumber,setStartRollNumber] = useState(entries[0]['HT']);
 
   const sendMessages = ()=> {
     entries.slice(messagesSent,messagesSent+10).map((entry,index)=> {
-      entry['Whatsapp No.']=String(entry['Whatsapp No.']);
-      entry['Roll No.']=String(entry['Roll No.']);
-      entry['Present Class']=String(entry['Present Class']);
-      if(entry['Whatsapp No.'].trim().length>0){
-        const message = `https://wa.me/${entry['Whatsapp No.'].trim()}?text=*BOB%20Wonderkid%20and%20Wonderparent%20Online/Offline%20Competition%20Prize%20Distribution%20Ceremony%202023*%0A%0ADear%20Parents,%0A%0A%0A%0A%0A${entry['Students Name'].trim().length>0&&('Name:%20'+entry['Students Name'])}${entry['Roll No.'].trim().length>0&&('%0aRoll%20No:%20'+entry['Roll No.'])}${entry['Present Class'].trim().length>0 && ('%0aClass:%20'+entry['Present Class'])}%0ADate:%20*Monday%202nd%20October*${entry['Time'].trim().length>0&&('%0aTime:%20'+entry['Time'])}%0A%0AVenue%20:%20*PRAYAG%20SANGEET%20SAMITI*%0ACompany%20Bagh%20PRAYAGRAJ%0A%0AWonderparents%20ceremony%20timings%20are%20the%20same%20if%20they%20have%20participated%20in%20the%20competition.%0A%0A*Coming%20on%20stage%20itself%20is%20a%20big%20motivation%20for%20any%20child,%20so%20please%20come%20and%20grace%20the%20occassion.*%0A%0ANOTE%20:%20All%20kids%20will%20come%20in%20School%20Uniform%0A%0ARegards%0APramesh%20Srivastava%0ACity%20Coordinator%0ABrainobrain%C2%A0Prayagraj`
+      entry['MOB']=String(entry['MOB']);
+      entry['HT']=String(entry['HT']);
+      entry['CLASS']=String(entry['CLASS']);
+      entry['NAME']=String(entry['NAME']);
+      if(entry['MOB'].trim().length>0){
+        console.log("entry", entry ); 
+        const message = `https://wa.me/${entry['MOB'].trim()}?text=*BOB%20Wonderkid%202024*%0A*Prize%20Distribution%20Ceremony*%0A%0ADear%20${entry['NAME'].trim()},%0AClass ${entry['CLASS'].trim()},%0AHT%20No.%20${entry['HT'].trim()},%0A%0ACongratulations!!%0AThe%20wait%20is%20over%20and%20it%20is%20time%20to%20declare%20the%20results%20of%20the%20BOB%20Wonderkid%20and%20Wonderparent%20Competition.%0A%0AWe%20take%20this%20opportunity%20to%20invite%20you%20for%20the%20prize%20distribution%20ceremony%20for%20the%20same.%0A%0A*Venue:%20KP%20Community%20Centre*%0ANear%20Medical%20Chauraha,%20Prayagraj%0A%0A*Day%20and%20Date:%20Sunday,%2010th%20November*%0A%0A*Time:%201%20PM%20to%203:30%20PM*%0A%0A*Notes:*%0A1.%20Your%20name%20will%20be%20called%20on%20stage%20to%20receive%20the%20Certificate%20/%20Award%0A2.%20Children%20need%20to%20*wear%20School%20Uniform*%0A%0ARegards,%0ATeam%20Brainobrain%20Prayagraj`
         window.open(message)
       }
     })
     setMessagesSent((prevMessagesSent)=>{return prevMessagesSent+10});
-    if(messagesSent+10<entries.length && entries[messagesSent+10]['Roll No.'].trim().length>0){
-      setStartRollNumber(entries[messagesSent+10]['Roll No.']);
+    if(messagesSent+10<entries.length && entries[messagesSent+10]['HT'].trim().length>0){
+      setStartRollNumber(entries[messagesSent+10]['HT']);
     }
   }
 
   const setRollNumberToStart = (e)=> {
-    const index=entries.findIndex(entry=>(entry['Roll No.']==e.target.value.trim()));
+    const index=entries.findIndex(entry=>(entry['HT']==e.target.value.trim()));
     if(index && index >=0) {
       setMessagesSent(index);  
       setStartRollNumber(e.target.value.trim());
@@ -53,10 +55,10 @@ function App() {
               {entries.slice(0,messagesSent).map((entry,index)=>(
                 <tr key={index}>
                   <td>
-                    {entry['Roll No.']}
+                    {entry['HT']}
                   </td>
                   <td>
-                    {entry['Students Name']}
+                    {entry['NAME']}
                   </td>
               </tr>
               ))}
